29ffc15a5cf3ee16a8daf81db17714990d3fc3ac
Laurent Chedanne <laurent@chedanne.pro>
2012-12-13 00:39:44 +0100

    Activation datepicker sur rel=date

--- js/application.js
+++ js/application.js
@@ -25,6 +25,8 @@ $(document).ready(function() {
 	    
 	    $('#recherche_avancee').searchbox();
 	    
+	    $('input[rel="date"]').datepicker({});
+	    
 	    //Scroll to form error
 	    $('form .alert-form').each(function(i, item) {
 	    	if (i == 0) {
