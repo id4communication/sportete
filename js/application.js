bb28ed32c5cde7aa450099d0d7f83f0937fb47fe
Laurent Chedanne <laurent@chedanne.pro>
2012-12-16 20:06:14 +0100

    Srollbox Bootstrap Oxygen Vignette séjour

--- js/application.js
+++ js/application.js
@@ -4,42 +4,9 @@
 
 $(document).ready(function() {
 	try {
-	
-		// Disable certain links in docs
-	    $('section [href^=#]').click(function (e) {
-	      e.preventDefault();
-	    });
-	
-	    // make code pretty
-	    window.prettyPrint && prettyPrint();
-	
-	    // tooltip / popover
-	    $('[rel="tooltip"]').tooltip();
-	    $('[rel="popover"]').popover();
-	
-	    // carousel
-	    $('[data-toggle="carousel"]').each(function() {
-	    	$(this).carousel( {
-	    		interval: $(this).attr('data-interval')
-	    	});
-	    });
-	    
 	    if (!(typeof($('#recherche_avancee').searchbox) == undefined)) {
 	    	$('#recherche_avancee').searchbox();
 	    }
-    	$('input[rel="date"]').each(function() {
-    		 if (!(typeof($(this).datepicker) == undefined)) {
-    			 $(this).datepicker({});
-    		 }
-    	});
-	    
-	    //Scroll to form error
-	    $('form .alert-form').each(function(i, item) {
-	    	if (i == 0) {
-	    		$.scrollTo($(this), 1000, { offset: -400 });
-	    	}
-	    });
-	    
 	} catch (e) {
 		console.log(e);
 	}
